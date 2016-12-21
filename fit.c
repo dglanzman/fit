7ba2811c050a7f777cd97973c84df6ff2b2a17b5
David Glanzman <gland@seas.upenn.edu>
2016-12-21 00:57:31 -0500

    update

--- fit.c
+++ fit.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_linalg.h>
 
 #define BLOCK_SIZE 128
 
@@ -54,24 +56,63 @@ void get_data_stdin(gsl_vector ** x, gsl_vector ** y) {
         gsl_vector_set(*x, i, raw_data.data[i * raw_data.w]);
         gsl_vector_set(*y, i, raw_data.data[i * raw_data.w + 1]);
     }
+    free(raw_data.data);
+}
+
+void least_squares(gsl_vector * x, gsl_vector * y, gsl_vector * c) {
+
+    int order = c->size; 
+
+    // generate polynomial model
+    gsl_matrix * model = gsl_matrix_alloc(x->size, order + 1);
+    gsl_vector * powers = gsl_vector_alloc(x->size);
+    gsl_vector_set_all(powers, 1);
+    for (int i = 0; i <= order; i++) {
+        gsl_matrix_set_col(model, i, powers);
+        gsl_vector_mul(powers, x);
+    }
+
+    // least-squares method is X'y = X'Xc
+    // calculate b = X'y
+    gsl_vector * b = gsl_vector_alloc(x->size);
+    gsl_blas_dgemv(CblasTrans, 1, model, y, 0, b);
+
+    // calculate A = X'X
+    gsl_matrix * A = gsl_matrix_alloc(x->size, x->size);
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1, model, model, 0, A);
+ 
+    // we now solve the Ac = b system for the coefficients c
+    gsl_permutation * p = gsl_permutation_alloc(x->size);
+    int sign;
+    gsl_linalg_LU_decomp(A, p, &sign);
+    gsl_linalg_LU_solve(A, p, b, c);
+
+
+    /* free memory
+    gsl_matrix_free(model);
+    gsl_vector_free(powers);
+    gsl_vector_free(b);
+    gsl_matrix_free(A);
+    gsl_permutation_free(p); */
 }
 
 int main() {
+
+    // read in x and y column
     gsl_vector * x, * y;
     int order;
     scanf("order=%d\n", &order);
     get_data_stdin(&x, &y);
-    gsl_vector_fprintf(stdout, x, "[%f]");
+
+    // find least-squares coefficients
+    gsl_vector * c = gsl_vector_alloc(order + 1);
+    least_squares(x, y, c);
+
+    gsl_vector_fprintf(stdout, c, "%f");
     printf("\n");
-    gsl_vector_fprintf(stdout, y, "[%f]");
-    printf("\n");
-    gsl_matrix * A = gsl_matrix_alloc(x->size, order + 1);
-    gsl_vector * powers = gsl_vector_alloc(x->size);
-    gsl_vector_set_all(powers, 1);
-    for (int i = 0; i <= order; i++) {
-        gsl_vector_view col = gsl_matrix_column(A, i);
-        gsl_vector_memcpy(&(col.vector), powers);
-        gsl_vector_mul(powers, x);
-    }
-    gsl_matrix_fprintf(stdout, A, "[%f]");
+
+    gsl_vector_free(x);
+    gsl_vector_free(y);
+    gsl_vector_free(c);
 }
+
--- makefile
+++ makefile
@@ -1,5 +1,8 @@
 fit: fit.c
-	cc -o fit `pkg-config --libs gsl` fit.c
+	cc -O2 -o fit `pkg-config --libs gsl` fit.c
+
+debug: fit.c
+	cc -g -O1 -o fit `pkg-config --libs gsl` fit.c
 
 clean:
 	rm -f fit
